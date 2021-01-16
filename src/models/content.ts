1951b7017aa7b7d0dc4ca9b9a6f47d077f4987c6
jdedwards3 <james.d.edwards.3@gmail.com>
2021-01-16 12:58:09 -0500

    undo sort by modified date

--- src/models/content.ts
+++ src/models/content.ts
@@ -288,10 +288,8 @@ export default class Content {
 
     posts.sort(
       (first: Partial<Content>, second: Partial<Content>) =>
-        new Date(
-          (second.modifiedDate ?? second.createdDate) as string
-        ).getTime() -
-        new Date((first.modifiedDate ?? first.createdDate) as string).getTime()
+        new Date(second.createdDate as string).getTime() -
+        new Date(first.createdDate as string).getTime()
     );
 
     return posts;
