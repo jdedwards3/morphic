499bc01cb6378cc54a0c82d3589448ec9e4b8e5d
James Edwards <james@jamesedwards.net>
2022-01-05 21:40:36 -0500

    rename

--- src/morphic.ts
+++ src/morphic.ts
@@ -6,11 +6,9 @@ import { pathUtil } from "./utils/pathUtil.js";
 import IArchiveTypeDisplayMap from "./interfaces/IArchiveTypeDisplayMap.js";
 import IArchiveType from "./interfaces/IArchiveType.js";
 import fs from "fs-extra";
-import IConfig from "./interfaces/IConfig.js";
 import ContentGuidWriter from "./models/contentGuidWriter.js";
 import ServiceWorkerBuilder from "./models/serviceWorkerBuilder.js";
 
-async function main(config: IConfig) {
   await fs.mkdirs(`${config.folders.output.path}`);
 
   await pathUtil.cacheOutputPaths(config);
@@ -72,49 +70,15 @@ async function main(config: IConfig) {
     await siteUtil.createRssFeed(config, contentPaths, archiveTypeDisplayMap);
   }
 
-  if (config.folders.rootFiles) {
-    await fs.copy(
-      `${config.folders.rootFiles.path}`,
-      `${config.folders.output.path}/`,
-      { overwrite: true }
-    );
+  await fs.copy(
+    `${config.folders.output.path}/`,
+    { overwrite: true }
+  );
+
+
   }
 
-  if (!config.sass.enabled && config.folders.styles.copyToOutput) {
-    await fs.copy(
-      `${config.folders.styles.path}`,
-      `${config.folders.output.path}/${
-        config.folders.styles.path.split(config.folders.site.path)[1]
-      }`,
-      { overwrite: true }
-    );
   }
 
-  if (!config.typescript.enabled && config.folders.scripts.copyToOutput) {
-    await fs.copy(
-      `${config.folders.scripts.path}`,
-      `${config.folders.output.path}/${
-        config.folders.scripts.path.split(config.folders.site.path)[1]
-      }`,
-      { overwrite: true }
-    );
-  }
-
-  for (const folder of Object.keys(config.folders)) {
-    if (config.folders[folder].copyToOutput) {
-      await fs.copy(
-        `${config.folders[folder].path}`,
-        `${config.folders.output.path}/${
-          config.folders[folder].path.split(config.folders.site.path)[1]
-        }`,
-        { overwrite: true }
-      );
-    }
-  }
-
-  await siteUtil.cleanOldOutputFiles(config);
-
-  console.log("Files Generated.");
 }
 
-export { main };
